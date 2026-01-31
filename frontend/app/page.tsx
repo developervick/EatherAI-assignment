6b05c210890d831a9b76f367c810c19f697e0b56
Vivek Kustwar <developervick@gmail.com>
2026-01-31 21:57:40 +0530

    project setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col py-32 px-16 bg-black">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col bg-black mt-48 gap-6 self-center justify-self-start text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Eather AI assignment
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Welcome to the new project
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
