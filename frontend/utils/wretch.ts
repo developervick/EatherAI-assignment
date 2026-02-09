6d1bdcac059cdda7d2ad7a9b6d1964745840287b
Vivek Kustwar <developervick@gmail.com>
2026-02-09 19:41:57 +0530

    final commit

--- frontend/utils/wretch.ts
+++ frontend/utils/wretch.ts
@@ -1,8 +1,8 @@
 import wretch from "wretch"
 
+const BackendHost = process.env.NEXT_PUBLIC_API_BASE_URL
 
-
-export const api = wretch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`, {
+export const api = wretch(`${BackendHost}/api/v1`, {
     mode: "cors",
     content: "application/json",
 })
