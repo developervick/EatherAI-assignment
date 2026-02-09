4da12963f26a4aa2dd41afdc096bc408f2a17cfe
Vivek Kustwar <developervick@gmail.com>
2026-02-09 19:37:33 +0530

    Wretch fixed

--- frontend/utils/wretch.ts
+++ frontend/utils/wretch.ts
@@ -2,7 +2,7 @@ import wretch from "wretch"
 
 
 
-export const api = wretch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1`, {
+export const api = wretch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`, {
     mode: "cors",
     content: "application/json",
 })
